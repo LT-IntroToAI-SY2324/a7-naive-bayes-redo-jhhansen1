414fdd0236fc3932ee70df99b32df79631ae6d18
Jacob <jhtruxal@cps.edu>
2024-03-20 11:42:43 -0500

    worked on A7

--- a6.py
+++ a6.py
@@ -54,22 +54,25 @@ class BayesClassifier:
 
         # files now holds a list of the filenames
         # self.training_data_directory holds the folder name where these files are
-        
+        # fName = files[0]
+        # print(fName)
 
         # stored below is how you would load a file with filename given by `fName`
         # `text` here will be the literal text of the file (i.e. what you would see
         # if you opened the file in a text editor
         # text = self.load_file(os.path.join(self.training_data_directory, fName))
-
+        text = self.load_file(os.path.join(self.training_data_directory))
+        print(text)
 
         # *Tip:* training can take a while, to make it more transparent, we can use the
         # enumerate function, which loops over something and has an automatic counter.
         # write something like this to track progress (note the `# type: ignore` comment
         # which tells mypy we know better and it shouldn't complain at us on this line):
-        # for index, filename in enumerate(files, 1): # type: ignore
-        #     print(f"Training on file {index} of {len(files)}")
+        for index, filename in enumerate(files, 1): # type: ignore
+            print(f"Training on file {index} of {len(files)}")
         #     <the rest of your code for updating frequencies here>
-
+            text = self.load_file(os.path.join(self.training_data_directory, filename))
+            print(text)
 
         # we want to fill pos_freqs and neg_freqs with the correct counts of words from
         # their respective reviews
@@ -88,7 +91,11 @@ class BayesClassifier:
         # those tokens. We've asked you to write a function `update_dict` that will make
         # your life easier here. Write that function first then pass it your list of
         # tokens from the file and the appropriate dictionary
-        
+        print(f"positive? {filename.startswith(self.pos_file_prefix)}")
+        print(f"negative? {filename.startswith(self.neg_file_prefix)}")
+        tokens=self.tokenize(text)
+        print(tokens)
+        self.update_dict(tokens, self.pos_freqs)
 
         # for debugging purposes, it might be useful to print out the tokens and their
         # frequencies for both the positive and negative dictionaries
